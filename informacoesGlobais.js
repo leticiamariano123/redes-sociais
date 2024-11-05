57a3a76db6576e674c62d62f9b039b1efadf3989
leticiamariano123 <127504693+leticiamariano123@users.noreply.github.com>
2024-11-05 09:09:25 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,9 +1,19 @@
-const url=‘https://raw.githubusercontent.com/s.jsoguilhermeonrails/api/main/dados-globain’
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais() {
-    const res = await fetch(url);
-    const dados = await res.json();
-    console.log(dados);
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas =(dados.total_pessoas_conectadas) /1e9
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
+
+    
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-visualizarInformacoesGlobais ()
+vizualizarInformacoesGlobais()
